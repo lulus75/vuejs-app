667d73b226387a643a9951fdfb5a7ba913549bcc
Kimelyne <kimelyne.servais@epita.fr>
2017-10-01 11:15:51 +0200

    create add tesk method

--- scripts/main.js
+++ scripts/main.js
@@ -10,6 +10,19 @@
 			]
 		},
 		methods: {
+
+			addTask: function () {
+				var getLastId = -1;
+				if(this.tasks.length !== 0){
+						getLastId = this.tasks[this.tasks.length-1].id
+				}
+      this.tasks.push({
+        id: getLastId+1,
+        title: this.taskTitle,
+        done: false
+      })
+      this.task = ''
+    },
 		getUndoneTask() {
 			return this.tasks.filter((task) => !task.done).length;
 		}
